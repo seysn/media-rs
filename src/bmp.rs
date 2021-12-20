0a392bafc5661b815898b858aa2084bfdfc0b683
Nicolas SEYS <seys.nicolas@gmail.com>
2021-12-20 23:48:44 +0100

    Reduce line size of read method

--- src/bmp.rs
+++ src/bmp.rs
@@ -62,42 +62,26 @@ impl BMP {
     pub fn read(filename: String) -> Result<BMP, String> {
         let mut file = File::open(filename).unwrap();
 
-        let signature = Signature::from_u16(read_u16(&mut file, Endianness::BigEndian)?)?;
-        let file_size = read_u32(&mut file, Endianness::LittleEndian)?;
-        let reserved1 = read_u16(&mut file, Endianness::BigEndian)?;
-        let reserved2 = read_u16(&mut file, Endianness::BigEndian)?;
-        let offset = read_u32(&mut file, Endianness::LittleEndian)?;
         let bitmap_file_header = BitmapFileHeader{
-            signature,
-            file_size,
-            reserved1,
-            reserved2,
-            offset
+            signature: Signature::from_u16(read_u16(&mut file, Endianness::BigEndian)?)?,
+            file_size: read_u32(&mut file, Endianness::LittleEndian)?,
+            reserved1: read_u16(&mut file, Endianness::BigEndian)?,
+            reserved2: read_u16(&mut file, Endianness::BigEndian)?,
+            offset: read_u32(&mut file, Endianness::LittleEndian)?,
         };
 
-        let dib_header_size = read_u32(&mut file, Endianness::LittleEndian)?;
-        let width = read_u32(&mut file, Endianness::LittleEndian)?;
-        let height = read_u32(&mut file, Endianness::LittleEndian)?;
-        let planes = read_u16(&mut file, Endianness::LittleEndian)?;
-        let bits_per_pixel = read_u16(&mut file, Endianness::LittleEndian)?;
-        let compression = read_u32(&mut file, Endianness::LittleEndian)?;
-        let image_size = read_u32(&mut file, Endianness::LittleEndian)?;
-        let horizontal_resolution = read_u32(&mut file, Endianness::LittleEndian)?;
-        let vertical_resolution = read_u32(&mut file, Endianness::LittleEndian)?;
-        let colors = read_u32(&mut file, Endianness::LittleEndian)?;
-        let important_colors = read_u32(&mut file, Endianness::LittleEndian)?;
         let dib_header = DIBHeader { 
-            dib_header_size,
-            width,
-            height,
-            planes,
-            bits_per_pixel,
-            compression,
-            image_size,
-            horizontal_resolution,
-            vertical_resolution,
-            colors,
-            important_colors
+            dib_header_size: read_u32(&mut file, Endianness::LittleEndian)?,
+            width: read_u32(&mut file, Endianness::LittleEndian)?,
+            height: read_u32(&mut file, Endianness::LittleEndian)?,
+            planes: read_u16(&mut file, Endianness::LittleEndian)?,
+            bits_per_pixel: read_u16(&mut file, Endianness::LittleEndian)?,
+            compression: read_u32(&mut file, Endianness::LittleEndian)?,
+            image_size: read_u32(&mut file, Endianness::LittleEndian)?,
+            horizontal_resolution: read_u32(&mut file, Endianness::LittleEndian)?,
+            vertical_resolution: read_u32(&mut file, Endianness::LittleEndian)?,
+            colors: read_u32(&mut file, Endianness::LittleEndian)?,
+            important_colors: read_u32(&mut file, Endianness::LittleEndian)?,
         };
 
         Ok(BMP { bitmap_file_header, dib_header })
